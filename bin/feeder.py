1bf384d83a8b96ab245a60b1c4438e1faca9564b
Frank Broy <f.r.f.broy@student.tudelft.nl>
2021-08-23 14:23:34 +0200

    Add URLextract and logging

--- bin/feeder.py
+++ bin/feeder.py
@@ -1,13 +1,99 @@
 import argparse
+import base64
 import configparser
 import json
 import logging
+import signal
 import sys
 
 import feedparser
+import newspaper
 import redis
+import validators
+from newspaper.article import ArticleException
 from pyail import PyAIL
 from trafilatura import feeds
+from urlextract import URLExtract
+
+
+def urlExtract(urls, link, origin):
+    for url in urls:
+        # If the url is not valid, drop it and continue
+        if not validators.url(url):
+            continue
+    
+        output = {}
+        output['source'] = ailurlextract
+        output['source-uuid'] = uuid
+        output['default-encoding'] = 'UTF-8'
+
+        output['meta'] = {}
+        output['meta']['atom-rss-link'] = link
+
+        output['meta']['atom-rss:url-extracted'] = url
+
+        signal.alarm(10)
+        try:
+            article = newspaper.Article(url)
+        except TimeoutError:
+            if args.verbose:
+                logging.error(f"Timeout reached for {url}")
+            continue
+        else:
+            signal.alarm(0)
+
+        # Caching
+        if r.exists(f"cu:{base64.b64encode(url.encode())}"):
+            if args.verbose:
+                logging.info(f"URL {url} already processed")
+            if not args.nocache:
+                continue
+        else:
+            r.set(f"cu:{base64.b64encode(url.encode())}", origin)
+            r.expire(f"cu:{base64.b64encode(url.encode())}", cache_expire)
+        
+        if args.verbose:
+            logging.info(f"Downloading and parsing {url}")
+
+        try:
+            article.download()
+            article.parse()
+        except ArticleException:
+            if args.verbose:
+                logging.error(f"Unable to download/parse {url}")
+            continue
+
+        output['data'] = article.html
+
+        nlpFailed = False
+
+        try:
+            article.nlp()
+        except:
+            if args.verbose:
+                logging.error(f"Unable to nlp {url}")
+            nlpFailed = True
+
+            if args.verbose:
+                logging.info("Uploading the URL to AIL...\n")
+                logging.info(json.dumps(output, indent=4, default=str))
+            pyail.feed_json_item(output['data'], output['meta'], ailurlextract, uuid)
+            continue
+    
+        if nlpFailed:
+            continue
+        
+        output['meta']['newspaper:text'] = article.text
+        output['meta']['newspaper:authors'] = article.authors
+        output['meta']['newspaper:keywords'] = article.keywords
+        output['meta']['newspaper:publish_date'] = article.publish_date
+        output['meta']['newspaper:top_image'] = article.top_image
+        output['meta']['newspaper:movies'] = article.movies
+
+        if args.verbose:
+            logging.info("Uploading the URL to AIL...\n")
+            logging.info(json.dumps(output, indent=4, default=str))
+        pyail.feed_json_item(output['data'], output['meta'], ailurlextract, uuid)
 
 
 logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s:%(message)s', level=logging.INFO, datefmt='%I:%M:%S')
@@ -50,17 +136,18 @@ except Exception as e:
 parser = argparse.ArgumentParser()
 parser.add_argument("--verbose", help="verbose output", action="store_true")
 parser.add_argument("--nocache", help="disable cache", action="store_true")
+parser.add_argument("--urlextract", help="extract urls", action="store_true")
 args = parser.parse_args()
 
-
-# TODO: URLextract
 with open("links.txt") as f:
     for link in f.readlines():
+        # Find feeds from the given link
         # feedUrls = feeds.find_feed_urls(link)
         feedUrls = []
         feedUrls.append(link.replace('\n', ''))
         for url in feedUrls:
-            print(url)
+            if args.verbose:
+                logging.info(url)
             if r.exists(f"c:{url}"):
                 if args.verbose:
                     logging.info(f"URL {url} already processed")
@@ -82,7 +169,11 @@ with open("links.txt") as f:
             o['meta'] = {}
             o['meta']['url'] = url
 
+            extractor = URLExtract()
+
             # Entries
+            if args.verbose:
+                logging.info("Extracting entries...")
             o['meta']['entries'] = []
             for entry in result['entries']:
                 if r.exists(f"c:{entry['link']}"):
@@ -106,9 +197,21 @@ with open("links.txt") as f:
                     e['meta']['id'] = entry['id']
                 if 'summary' in entry:
                     e['meta']['summary'] = entry['summary']
+                    urls = extractor.find_urls(entry['summary'])
+                    if len(urls) != 0 and args.urlextract:
+                        if args.verbose:
+                            logging.info("Extracting URLs...")
+                        urlExtract(urls, link, entry['summary'])
+
                 e['content'] = ''
                 if 'content' in entry:
                     e['content'] = entry['content'][0]['value']
+                    urls = extractor.find_urls(entry['content'][0]['value'])
+                    if len(urls) != 0 and args.urlextract:
+                        if args.verbose:
+                            logging.info("Extracting URLs...")
+                        urlExtract(urls, link, entry['content'][0]['value'])
+
                 if 'authors' in entry:
                     e['meta']['authors'] = entry['authors']
                 if 'published' in entry:
@@ -127,6 +230,10 @@ with open("links.txt") as f:
                 pyail.feed_json_item(e['content'], e['meta'], ailfeedertype, uuid)
                 o['meta']['entries'].append(e['meta']['link'])
 
+            if args.verbose:
+                logging.info("Done extracting entries.")
+                logging.info("Extracting feeds...")
+
             # Feeds
             o['meta']['feed'] = {}
             if 'title' in result['feed']:
@@ -142,6 +249,10 @@ with open("links.txt") as f:
                     l['rel'] = link['rel']
                     l['href'] = link['href']
                     o['meta']['feed']['links'].append(l)
+
+            if args.verbose:
+                logging.info("Done extracting feeds.")
+                logging.info("Extracting other data...")
 
             # Others
             if 'headers' in result:
@@ -159,6 +270,9 @@ with open("links.txt") as f:
             if 'etag' in result:
                 o['meta']['etag'] = result['etag']
 
-            print(json.dumps(o, indent=4, default=str))
+            if args.verbose:
+                logging.info("Done extracting everything!")
+                logging.info(json.dumps(o, indent=4, default=str))
+                logging.info("Uploading to AIL...")
             # TODO: should url be the content?
             pyail.feed_json_item(url, o['meta'], ailfeedertype, uuid)
